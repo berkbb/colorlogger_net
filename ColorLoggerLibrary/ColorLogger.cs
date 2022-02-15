c54a21dca8b25109da9907563a6460af6bfe9ff7
berkbb <berkbaba_dev@outlook.com>
2022-02-15 21:57:51 +0300

    First initialize.

---
+++ ColorLoggerLibrary/ColorLogger.cs
@@ -0,0 +1,173 @@
+﻿namespace ColorLoggerLibrary;
+using System.Diagnostics;
+using System.Text;
+
+public class ColorLogger
+{
+    /// <summary>
+    /// Prints to console with color. Like Console.WriteLine() with colored foreground.
+    ///Note that coloring only available on external OS terminals. Visual Studio and Visual Studio Code does not support coloring.
+    /// </summary>
+    /// <param name="msg">Message.</param>
+    /// <param name="lvl">Log level.</param>
+    public void Print(string msg, LogLevel lvl)
+    {
+        Console.WriteLine(); //Empty space.
+
+        //Foreground selector.
+        switch (lvl)
+        {
+            case LogLevel.verbose:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+            case LogLevel.debug:
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            case LogLevel.info:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            case LogLevel.warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogLevel.error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+        }
+        StringBuilder title = new StringBuilder(); // String builder for title.
+        var msgTitle = $"[{lvl}]"; // Title
+        var msgOut = $"> {msg}"; // Message
+
+        // Append to title
+        title.Append(msgTitle);
+        for (int i = 0; i < Console.WindowWidth - msgTitle.Length - 1; i++)
+        {
+            title.Append("-");
+
+        }
+        var upper = title.ToString(); // Upper
+
+        // Calculating the lines.
+        double divide = Convert.ToDouble(msgOut.Length) / Convert.ToDouble(Console.WindowWidth);
+        divide = Math.Ceiling(divide);
+
+        //Print upper title.
+        Console.WriteLine(upper);
+
+        if (divide <= 1) // If 0 (empty) or 1 line.
+        {
+            var newOut = $"| {msgOut}";
+            var printCount = Console.WindowWidth - newOut.Length;
+
+            for (int i = 0; i < printCount; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"| {msgOut}");
+                }
+                else if (i != printCount - 1)
+                {
+                    Console.Write(" ");
+                }
+
+                else
+                {
+                    Console.Write("|");
+
+                }
+
+            }
+            Console.WriteLine();
+        }
+
+        else // 2 or more line.
+        {
+            int stringStartIndex = 0;
+
+            for (int i = 0; i < divide; i++)
+            {
+
+                if (i == 0)
+                {
+                    var firstSub = msgOut.Substring(0, Console.WindowWidth - 4);
+                    stringStartIndex = firstSub.Length - 2;
+                    var newOut = $"| {firstSub} |";
+
+
+                    Console.WriteLine(newOut);
+
+                }
+                else
+                {
+
+                    var printCount = Console.WindowWidth;
+                    for (int j = 0; j < printCount; j++)
+                    {
+                        if (j == 0)
+                        {
+                            Console.Write($"|");
+                        }
+                        else if (j != printCount - 1)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        else
+                        {
+                            Console.Write("|");
+
+                        }
+
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
+        }
+
+
+        // Print footer.
+        for (int i = 0; i < upper.Length; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine(); //Empty space.
+
+        // Reset foreground.
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+
+
+}
+/// <summary>
+/// Log level enum.
+/// </summary>
+public enum LogLevel
+{
+    /// <summary>
+    /// Verbose - yellow
+    /// </summary>
+    verbose,
+    /// <summary>
+    /// Debug - cyan.
+    /// </summary>
+    debug,
+    /// <summary>
+    /// İnfo - green.
+    /// </summary>
+    info,
+    /// <summary>
+    /// Warning -- yellow.
+    /// </summary>
+    warning,
+    /// <summary>
+    /// Error - red.
+    /// </summary>
+    error,
+    /// <summary>
+    /// Standart text print - white.
+    /// </summary>
+    unknown
+
+}
---
+++ ShowCase/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ColorLoggerLibrary;
+
+ColorLogger logger = new ColorLogger();
+logger.Print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", LogLevel.debug);
+logger.Print("hello world !", LogLevel.verbose);
+logger.Print("love dotnet :)", LogLevel.info);
+logger.Print("hey check that!", LogLevel.warning);
+logger.Print("please use c# !", LogLevel.error);
+logger.Print("dotnet rocks !", LogLevel.unknown);
